be2ca9f2039700d516805154a1bdb03935a4f899
Gustavo Dagostini <gustavomolindagostini@gmail.com>
2025-07-17 21:50:54 -0300

    Adição dos exemplos e ferramentas de análise parametrica

--- perda_de_carga.py
+++ perda_de_carga.py
@@ -54,29 +54,7 @@ def perda_de_carga(L, D, u, f, rho):
     # Cálculo da perda de carga (Pa)
     return f * (L / D) * (rho * u**2 / 2)
 
-densidade = 925 # kg/m³
-mu = 0.0945 # Pa.s
-D_int_pol = 24 #poleg
-D_int_m = 24*2.54/100 #poleg
 
-vazao_volumetrica = 0.59 # m³/s
-vazao_massica = vazao_volumetrica*densidade  # kg/s
-
-# Área da seção transversal
-A = np.pi * (D_int_m / 2)**2
-
-# Velocidade média do fluido no tubo (m/s)
-velocidade = vazao_volumetrica / A
-
-Re = Reynolds(rho=densidade,u=velocidade,D=D_int_m,mu=mu)
-c = f_atrito(Re, D=D_int_m, epsilon = 45e-6, f_init=0.02, tol=1e-6, max_iter=100)
-
-
-d = perda_de_carga(L=1, D = D_int_m , u=velocidade, f=c, rho=densidade)
-
-
-
-a=2
 
 
 
